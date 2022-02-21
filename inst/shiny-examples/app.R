50bcfb7fee96f5cf528d2ec8eded6346ce86e0a4
Gautier Stoll <gautier.stoll@curie.fr>
2022-02-21 19:23:50 +0100

    Simple automatic shiny app

--- inst/shiny-examples/app.R
+++ inst/shiny-examples/app.R
@@ -6,30 +6,41 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel("Raw Data",
                  fileInput("RawFile", "Choose CSV File", accept = ".csv"),
-                 checkboxGroupInput(
-                   "ObservationList",
-                   label = tableOutput("Observations"),
-                   choices = NULL,
-                   selected = NULL,
-                   inline = FALSE,
-                   width = NULL,
-                   choiceNames = NULL,
-                   choiceValues = NULL
-                 )),
-    mainPanel("Observations",tableOutput("Observations"))
+                 ),
+    mainPanel("Analysis",textOutput("StatOutput"))
+   ## mainPanel("Observations",uiOutput("Observations")
+              ##checkboxGroupInput(
+      ##"choiceObservation",
+      ##label = "Select observations",
+      ##choices = names(tableOutput("Observations")),
+      ##selected = NULL,
+      ##inline = FALSE,
+      ##width = NULL,
+      ##choiceNames = NULL,
+      ##choiceValues = NULL
+    #)
+    )
   )
-)
+
 
 # Define server logic 
 server <- function(input, output) {
-  output$Observations <- renderTable({
-      file <- input$RawFile
-      ext <- tools::file_ext(file$datapath)
-      req(file)
-      validate(need(ext == "csv", "Please upload a csv file"))
-      RawMetaData = new("RawMetaboData",fileName = file$datapath)
-      names(RawMetaData@data)
-    })
+  ##output$Observations <- renderUI({
+  ##    file <- input$RawFile
+  ##    ext <- tools::file_ext(file$datapath)
+  ##    req(file)
+  ##    validate(need(ext == "csv", "Please upload a csv file"))
+  ##    RawData = new("RawMetaboData",fileName = file$datapath)
+  ##    checkboxGroupInput("obsChoice",label = "Select observations",choices = names(RawData@data))
+  ##  })
+  output$StatOutput =  renderText({
+    file <- input$RawFile
+    ext <- tools::file_ext(file$datapath)
+    req(file)
+    validate(need(ext == "csv", "Please upload a csv file"))
+    RawData = new("RawMetaboData",fileName = file$datapath)
+    
+  })
   
 }
 
