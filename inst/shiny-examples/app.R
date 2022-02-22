005e3d5687f2c8103b98e195df66904a9ea70b3c
Gautier Stoll <gautier.stoll@curie.fr>
2022-02-22 17:44:43 +0100

    Update shiny application

--- inst/shiny-examples/app.R
+++ inst/shiny-examples/app.R
@@ -1,46 +1,41 @@
 library(shiny)
+library(shinyFiles)
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   titlePanel("ResStatMetabo"),
-  sidebarLayout(
-    sidebarPanel("Raw Data",
-                 fileInput("RawFile", "Choose CSV File", accept = ".csv"),
-                 ),
-    mainPanel("Analysis",textOutput("StatOutput"))
-   ## mainPanel("Observations",uiOutput("Observations")
-              ##checkboxGroupInput(
-      ##"choiceObservation",
-      ##label = "Select observations",
-      ##choices = names(tableOutput("Observations")),
-      ##selected = NULL,
-      ##inline = FALSE,
-      ##width = NULL,
-      ##choiceNames = NULL,
-      ##choiceValues = NULL
-    #)
+  fluidRow(
+    column(4,
+    tags$p("Select raw data file."),
+    shinyFilesButton("rawFile", "Raw Data File", "Select a raw data file",multiple = F, viewtype = "detail"),
+    tags$hr(),
+    tags$p("Select annotation file"),
+    shinyFilesButton("annotFile", "Annotation File", "Select an annotation file",multiple = F, viewtype = "detail")),
+    column(4,"Observations",
+              tableOutput("Observations")),
+    column(4,"Annotations",tableOutput("Annotations"))
+    
     )
   )
 
 
 # Define server logic 
-server <- function(input, output) {
-  ##output$Observations <- renderUI({
-  ##    file <- input$RawFile
-  ##    ext <- tools::file_ext(file$datapath)
-  ##    req(file)
-  ##    validate(need(ext == "csv", "Please upload a csv file"))
-  ##    RawData = new("RawMetaboData",fileName = file$datapath)
-  ##    checkboxGroupInput("obsChoice",label = "Select observations",choices = names(RawData@data))
-  ##  })
-  output$StatOutput =  renderText({
-    file <- input$RawFile
-    ext <- tools::file_ext(file$datapath)
-    req(file)
-    validate(need(ext == "csv", "Please upload a csv file"))
-    RawData = new("RawMetaboData",fileName = file$datapath)
-    
-  })
+server <- function(input, output,session) {
+  volumes = c(home = fs::path_home(),wd = fs::path_wd(),getVolumes()())
+  shinyFileChoose(input, "rawFile", defaultRoot = "home",roots = volumes, session = session,filetypes = c("csv"))
+  shinyFileChoose(input, "annotFile", defaultRoot = "home",roots = volumes, session = session,filetypes = c("csv"))
+  output$Observations =  renderTable({
+    if (is.integer(input$rawFile)) {c(" ")} else {
+    print((parseFilePaths(volumes, input$rawFile))$datapath)
+    RawData = new("RawMetaboData",fileName = parseFilePaths(volumes, input$rawFile)$datapath)
+    names(RawData@data)}
+  },colnames=F)
+  output$Annotations = renderTable({
+    if (is.integer(input$annotFile)) {c(" ")} else {
+      print((parseFilePaths(volumes, input$annotFile))$datapath)
+      AnnotData = read.csv(file = parseFilePaths(volumes, input$annotFile)$datapath)
+      names(AnnotData)}
+  },colnames=F)
   
 }
 
