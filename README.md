693909e67d7564c838ea36370d1e6f6c5acf4844
Gautier Stoll <gautier.stoll@curie.fr>
2022-02-23 17:52:10 +0100

    Shiny application going on

--- README.md
+++ README.md
@@ -18,10 +18,25 @@ Some dependent packages may not be available directly. They have to be installed
 ```R
 install.packages("lubridate")
 ```
-
 [lme](https://cran.r-project.org/web/packages/nlme/index.html):
 ```R
 install.packages("nlme")
+```
+[shiny](https://cran.r-project.org/web/packages/shiny/index.html):
+```R
+install.packages("shiny")
+```
+[shinyFiles](https://cran.r-project.org/web/packages/shinyFiles/index.html):
+```R
+install.packages("shinyFiles")
+```
+[fs](https://cran.r-project.org/web/packages/fs/index.html):
+```R
+install.packages("fs")
+```
+[readxl](https://cran.r-project.org/web/packages/readxl/index.html):
+```R
+install.packages("readxl")
 ```
 
 ## HowTo
@@ -33,7 +48,7 @@ library(StatMetCage)
 ```R
 RawMetaData = new("RawMetaboData",fileName = "myData.csv")
 ```
-3. Create an analysis object that contains the observation of interests(eq "VO2" and "VCO2""). An annotation data frame can be included (eg `myAnnotation`) that must contains "Animal", "Date" and "Time" column, with qualitative annotation (eg "Treatment", "Mutation"), the other columns will be treated as numeric data, with a time dependence evaluated by spline:
+3. Create an analysis object that contains the observation of interests(eg "VO2" and "VCO2""). An annotation data frame can be included (eg `myAnnotation`) that must contains "Animal", "Date" and "Time" column, with qualitative annotation (eg "Treatment", "Mutation"), the other columns will be treated as numeric data, with a time dependence evaluated by spline:
 ```R
 AnalysisMeta = new("AnalysisMetaboData",rawData = RawMetaData,obs = c("VO2","VCO2"),annotation = myAnnotation,annotGroups = c("Treatment","Mutation"))
 ```
--- inst/shiny-examples/app.R
+++ inst/shiny-examples/app.R
@@ -10,7 +10,9 @@ ui <- fluidPage(
     shinyFilesButton("rawFile", "Raw Data File", "Select a raw data file",multiple = F, viewtype = "detail"),
     tags$hr(),
     tags$p("Select annotation file"),
-    shinyFilesButton("annotFile", "Annotation File", "Select an annotation file",multiple = F, viewtype = "detail")),
+    shinyFilesButton("annotFile", "Annotation File", "Select an annotation file",multiple = F, viewtype = "detail"),
+    tags$hr(),
+    actionButton("RunAnalysis","Run Analysis")),
     column(4,"Observations",
               tableOutput("Observations")),
     column(4,"Annotations",tableOutput("Annotations"))
@@ -23,20 +25,29 @@ ui <- fluidPage(
 server <- function(input, output,session) {
   volumes = c(home = fs::path_home(),wd = fs::path_wd(),getVolumes()())
   shinyFileChoose(input, "rawFile", defaultRoot = "home",roots = volumes, session = session,filetypes = c("csv"))
-  shinyFileChoose(input, "annotFile", defaultRoot = "home",roots = volumes, session = session,filetypes = c("csv"))
+  shinyFileChoose(input, "annotFile", defaultRoot = "home",roots = volumes, session = session,filetypes = c("xlsx"))
+  MetaboContainer = reactiveValues()
   output$Observations =  renderTable({
     if (is.integer(input$rawFile)) {c(" ")} else {
     print((parseFilePaths(volumes, input$rawFile))$datapath)
-    RawData = new("RawMetaboData",fileName = parseFilePaths(volumes, input$rawFile)$datapath)
-    names(RawData@data)}
+    MetaboContainer$RawData = new("RawMetaboData",fileName = parseFilePaths(volumes, input$rawFile)$datapath)
+  testCol = which(is.element(c("Date","Time","Animal No."),names(MetaboContainer$RawData@data)))
+  if (length(testCol) < 3) {MetaboContainer$RawData = NULL;c("Missing elements",c("Date","Time","Animal No.")[-testCol])} else
+    {names(MetaboContainer$RawData@data)[!is.element(names(MetaboContainer$RawData@data),c("Date","Time","Animal No."))]}}
   },colnames=F)
   output$Annotations = renderTable({
     if (is.integer(input$annotFile)) {c(" ")} else {
       print((parseFilePaths(volumes, input$annotFile))$datapath)
-      AnnotData = read.csv(file = parseFilePaths(volumes, input$annotFile)$datapath)
-      names(AnnotData)}
+      MetaboContainer$Annot = readxl::read_excel(path = parseFilePaths(volumes, input$annotFile)$datapath)
+      testCol = which(is.element(c("Date","Time","Animal","Group"),names(MetaboContainer$Annot)))
+      if (length(testCol) < 4) {MetaboContainer$Annot = NULL; c("Missing elements",c("Date","Time","Animal","Group")[-testCol])} else
+      names(MetaboContainer$Annot)[!is.element(names(MetaboContainer$Annot),c("Date","Time","Animal","Group"))]}
   },colnames=F)
-  
+  observeEvent(input$RunAnalysis,{
+    if (!is.null(MetaboContainer$Annot) & !is.null(MetaboContainer$RawData)) {
+      
+    }
+  })
 }
 
 shinyApp(ui = ui, server = server)
