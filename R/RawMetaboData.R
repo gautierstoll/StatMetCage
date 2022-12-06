b5420b9b740addb745f0d1dc70cbf65d0a589d81
gautierstoll <gautier.stoll@upmc.fr>
2022-12-06 14:52:32 +0100

    Optional .csv column separator

--- R/RawMetaboData.R
+++ R/RawMetaboData.R
@@ -17,15 +17,16 @@ setClass(Class = "RawMetaboData",
 #' Constructor of class RawMetaboData
 #'
 #' @param fileName .csv file containing raw data
+#' @param sepCSBV column separator for .csv file
 #' @export
 #'
 setMethod(f="initialize",
           signature = "RawMetaboData",
-          definition = function(.Object,fileName){
+          definition = function(.Object,fileName,sepCSV = ","){
             Data=scan(fileName,what="",sep="\n")
             tmpFirst = grep("Date",Data,fixed=T)
             .Object@header = Data[1:tmpFirst]
-            DataTable = lapply(Data[c(tmpFirst,((tmpFirst+2):length(Data)))],function(l){strsplit(l,split = ",")[[1]]})
+            DataTable = lapply(Data[c(tmpFirst,((tmpFirst+2):length(Data)))],function(l){strsplit(l,split = sepCSV)[[1]]})
             DataDF = as.data.frame(do.call("rbind",DataTable[-1]))
             names(DataDF) = DataTable[[1]]
             for (index in (1:length(DataDF[1,]))) {DataDF[,index] = as.character(DataDF[,index])}
