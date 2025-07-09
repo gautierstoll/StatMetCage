2e6266282af08fc7372a0160e6bb61f4eefbd3f4
Vincent Carbonnier <vincent.carbonnier@crc.jussieu.fr>
2025-07-09 09:05:17 +0200

    New version for setMethod initialize, which fill all slots in RawMetaboData Class

--- R/RawMetaboData.R
+++ R/RawMetaboData.R
@@ -26,19 +26,38 @@ setClass(Class = "RawMetaboData",
 #' Constructor of class RawMetaboData
 #'
 #' @param fileName .csv file(s) containing raw data
-#' @param sepCSV column separator for .csv file, "," by default
+#' @param sepCSV column separator for .csv file, by default is ";"
+#' @param sepDec Decimal separator for .csv file, by default is ","
 #' @export
 #'
 setMethod(f="initialize",
           signature = "RawMetaboData",
-          definition = function(.Object,fileName,sepCSV = ","){
-            Data=scan(fileName,what="",sep="\n")
-            tmpFirst = grep("Date",Data,fixed=T)
-            .Object@header = Data[1:tmpFirst]
-            DataTable = lapply(Data[c(tmpFirst,((tmpFirst+2):length(Data)))],function(l){strsplit(l,split = sepCSV)[[1]]})
-            DataDF = as.data.frame(do.call("rbind",DataTable[-1]))
-            names(DataDF) = DataTable[[1]]
+          definition = function(.Object,fileName,sepCSV = ";",sepDec=","){
+            Data=scan(fileName,what="",sep="\n",dec=sepDec,blank.lines.skip = FALSE)
+            Data <- Data[Data != ""] # Remove possible blank lines
+            tmpFirst <- grep("Date;Time", Data, useBytes = TRUE) # Find Data table start with Date;Time
+            # Meta info
+            tmp <- strsplit(Data[1:tmpFirst-1], sepCSV)
+            .Object@Date = as.Date(tmp[[1]][1], tryFormats = c("%d%m%Y","%Y-%m-%d", "%Y/%m/%d"))
+            .Object@Batch = tmp[[1]][2]
+            .Object@Device = tmp[[2]][2]
+            # Header data
+            .Object@header = as.data.frame(do.call("rbind", sapply(Data[4:tmpFirst-1], strsplit, sepCSV)))
+            colnames(.Object@header) <- .Object@header[1,]
+            .Object@header <- .Object@header[-c(1),]
+            # Main data
+            # Warning: Unit line create some error
+            DataTable = lapply(Data[tmpFirst+2:length(Data)],function(l){strsplit(l,split = sepCSV)[[1]]})
+            DataDF = as.data.frame(do.call("rbind",DataTable))
+            colnames(DataDF) = strsplit(Data[tmpFirst],sepCSV)[[1]]
             for (index in (1:length(DataDF[1,]))) {DataDF[,index] = as.character(DataDF[,index])}
-            .Object@data = DataDF
+            
+            .Object@data = DataDF %>% as_tibble %>% 
+              mutate(OriginDate = .Object@Date)
+            
+            .Object@header <- .Object@header %>% as_tibble %>%
+              mutate_at(3, function(x) as.numeric(gsub(",",".", x))) %>%
+              mutate(Date = DataDF[1,1], Time = DataDF[1,2])
+            
             return(.Object)
           })
