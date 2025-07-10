0e498409728c17862fd9a6aa5557b5723dd3adba
Vincent Carbonnier <vincent.carbonnier@crc.jussieu.fr>
2025-07-10 15:00:04 +0200

    Get out the filtering step

--- R/AnalysisMetaboData.R
+++ R/AnalysisMetaboData.R
@@ -134,11 +134,12 @@ setGeneric(
 setMethod(f="metaboRawPlot2",
           signature = "AnalysisMetaboData",
           definition = function(x,observation,group = "Group"){
-            gg <- ggplot(x@data %>% filter(!is.na(get(observation))), aes(x = RelDay, y = get(observation), color = get(group)))+
+            tmp <- x@data %>% filter(!is.na(get(observation)))
+            gg <- ggplot(tmp, aes(x = RelDay, y = get(observation), color = get(group)))+
               geom_line(alpha = 0.3, if('Animal No.' %in% colnames(x@data)){aes(group = `Animal No.`)}) +
               labs(y = observation, color = group) +
               ggtitle(observation) +
               geom_smooth(method = "loess", formula = 'y ~ x', span = 0.01)+
               theme_bw()
-            return(gg)
+            print(gg)
           })
