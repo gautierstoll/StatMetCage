89e2504a541ebadff2f2bc373558f0f4cf2933f7
Vincent Carbonnier <vincent.carbonnier@crc.jussieu.fr>
2026-03-26 10:47:35 +0100

    test commit

--- R/ResDailyMeanStatMetabo.R
+++ R/ResDailyMeanStatMetabo.R
@@ -14,6 +14,7 @@ setOldClass("TukeyHSD")
 setOldClass("multitcomp")
 NULL
 
+<<<<<<< HEAD
 # function plage
 plage <- function(value, i=0){
   res <- c()
@@ -36,6 +37,9 @@ plage <- function(value, i=0){
 
 
 
+=======
+test <- function(){print("test")}
+>>>>>>> 83e0ad0 (test commit)
 
 
 #' Class for linear modeling of temporal mean
